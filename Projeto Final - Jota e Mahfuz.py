68cced7d14991443aea453c12a07584a59366e09
jotapsmo <jota.psmo@outlook.com>
2020-05-18 17:11:06 -0300

    teste

--- Projeto Final - Jota e Mahfuz.py
+++ Projeto Final - Jota e Mahfuz.py
@@ -1,11 +1,18 @@
 import pygame
 import random 
-
-pygame.init()
+import os
+import sys
 
 #Definindo cores:
 BRANCO = (255, 255, 255)
 PRETO = (0, 0, 0)
 VERMELHO = (255, 0, 0) 
 VERDE = (0, 255, 0)
-AZUL = (0, 0, 255) 
+AZUL = (0, 0, 255)
+CINZA = (127, 127, 127)
+
+pygame.init() # Iniciando as rotinas do pygame
+
+surf = pygame.display.set_mode((500, 500))
+
+pygame.display.set_caption("Insira o nome do jogo aqui")
